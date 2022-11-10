7b5af2b2860b73525c9d397e2d5a6857de1a6b59
Germain Michaud <gmichaud.dev@gmail.com>
2022-11-10 13:18:23 +0100

    feat: structure App with all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Outlet } from '@tanstack/react-location';
+import { useEffect } from 'react';
+
+import Breadcrumbs from './components/Breadcrumbs';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
+import { useAuthStore } from './store/authStore';
+
 const App = () => {
-  return <></>;
+  const getUser = useAuthStore((state) => state.getUser);
+
+  useEffect(() => {
+    void getUser();
+  }, [getUser]);
+
+  return (
+    <>
+      <Header />
+      <Wrapper>
+        <Breadcrumbs />
+        <Outlet />
+      </Wrapper>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
