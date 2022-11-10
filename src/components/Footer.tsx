ae1850f8094a96f126c86401855574f088e40d64
Germain Michaud <gmichaud.dev@gmail.com>
2022-11-10 13:14:11 +0100

    feat: create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => (
+  <footer className="bg-gray-200 text-center lg:text-left">
+    <div className="text-gray-700 text-center p-4">
+      © {new Date().getFullYear()} Copyright:
+      <a className="text-gray-800" href="https://tailwind-elements.com/">
+        Share Video
+      </a>
+    </div>
+  </footer>
+);
+
+export default Footer;
